9caca513c4056ad413061df20bcff2d0523777d3
thomasgaillard <thomas.gaillard@me.com>
2014-04-09 12:44:09 +0200

    modal

--- VBoatGestion/ActiviteModalEmbarcation.h
+++ VBoatGestion/ActiviteModalEmbarcation.h
@@ -23,6 +23,8 @@
 @property (weak, nonatomic) IBOutlet UITextField *remarquesLoc;
 @property (weak, nonatomic) IBOutlet UITextField *hDebutLoc;
 @property (weak, nonatomic) IBOutlet UITextField *hFinLoc;
+@property (weak, nonatomic) IBOutlet UILabel *lblDeb;
+@property (weak, nonatomic) IBOutlet UILabel *lblFin;
 @property (weak, nonatomic) IBOutlet UITextField *nbPersonnesLoc;
 @property (weak, nonatomic) IBOutlet UILabel *typeOuNb;
 @property (weak, nonatomic) IBOutlet UILabel *nbPersonnesLocLabel;
@@ -30,11 +32,16 @@
 @property (weak, nonatomic) IBOutlet UIButton *stopLocBtn;
 @property (weak, nonatomic) IBOutlet UIButton *indispoEmbBtn;
 @property (weak, nonatomic) IBOutlet UIButton *dispoEmbBtn;
-@property (weak, nonatomic) IBOutlet UIButton *btnP;
-@property (weak, nonatomic) IBOutlet UIButton *btnM;
+@property (weak, nonatomic) IBOutlet UIButton *btn2;
+@property (weak, nonatomic) IBOutlet UIButton *btn3;
+@property (weak, nonatomic) IBOutlet UIButton *btn4;
+@property (weak, nonatomic) IBOutlet UIButton *btn5;
 
-- (IBAction)btnPlus:(id)sender;
-- (IBAction)btnMoins:(id)sender;
+- (IBAction)clickBtn2:(id)sender;
+- (IBAction)clickBtn3:(id)sender;
+- (IBAction)clickBtn4:(id)sender;
+- (IBAction)clickBtn5:(id)sender;
+
 - (IBAction)closeModal:(id)sender;
 - (IBAction)saveModalInfos:(id)sender;
 - (IBAction)startLoc:(id)sender;
