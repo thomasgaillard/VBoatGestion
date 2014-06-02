5808646d4a003e1daeb177f0f5cf099f2e16e1da
thomasgaillard <thomas.gaillard@me.com>
2014-06-02 12:28:31 +0200

    pas de facture

--- VBoatGestion/FacturationDetailsViewController.m
+++ VBoatGestion/FacturationDetailsViewController.m
@@ -25,11 +25,52 @@
     return self;
 }
 
+-(UIColor*)colorWithHexString:(NSString*)hex
+{
+    NSString *cString = [[hex stringByTrimmingCharactersInSet:[NSCharacterSet whitespaceAndNewlineCharacterSet]] uppercaseString];
+    
+    // String should be 6 or 8 characters
+    if ([cString length] < 6) return [UIColor grayColor];
+    
+    // strip 0X if it appears
+    if ([cString hasPrefix:@"0X"]) cString = [cString substringFromIndex:2];
+    
+    if ([cString length] != 6) return  [UIColor grayColor];
+    
+    // Separate into r, g, b substrings
+    NSRange range;
+    range.location = 0;
+    range.length = 2;
+    NSString *rString = [cString substringWithRange:range];
+    
+    range.location = 2;
+    NSString *gString = [cString substringWithRange:range];
+    
+    range.location = 4;
+    NSString *bString = [cString substringWithRange:range];
+    
+    // Scan values
+    unsigned int r, g, b;
+    [[NSScanner scannerWithString:rString] scanHexInt:&r];
+    [[NSScanner scannerWithString:gString] scanHexInt:&g];
+    [[NSScanner scannerWithString:bString] scanHexInt:&b];
+    
+    return [UIColor colorWithRed:((float) r / 255.0f)
+                           green:((float) g / 255.0f)
+                            blue:((float) b / 255.0f)
+                           alpha:1.0f];
+}
+
+
 - (void)viewDidLoad
 {
     AppDelegate* appDelegate  = [UIApplication sharedApplication].delegate;
     self.managedObjectContext = appDelegate.managedObjectContext;
     [self rafraichir];
+    
+     _aucuneFacturation.textColor = [self colorWithHexString:@"95a5a6"];
+     _aucuneFacturation.font = [UIFont fontWithName:@"LeagueGothic-Regular" size:26];
+    
     [super viewDidLoad];
     
 }
@@ -160,6 +201,7 @@
 -(void)selectedFacture:(Facture *)facture :(UITableView *)tableView :(NSUInteger*) indexPath :(NSMutableArray*) facturesArray
 {
         self.noFactures.hidden=YES;
+    self.noFacturesBg.hidden=YES;
         self.facturesArray=facturesArray;
         self.tableView=tableView;
         self.indexPath=indexPath;
@@ -170,6 +212,7 @@
 
 -(void)noFactures:(int)empty{
     self.noFactures.hidden=NO;
+    self.noFacturesBg.hidden=NO;
 }
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
